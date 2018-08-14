3131c6b173a21b49643e2fe7687321872ec0ce98
samueljohnwilson <samjwilson@gmail.com>
2018-08-14 17:38:19 +0000

    Add one-to-many association between tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sam", last_name: "Wilson", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "McDonald", hourly_rate: 60)
+@store2.employees.create(first_name: "Jim", last_name: "Davis", hourly_rate: 60)
+@store2.employees.create(first_name: "Bob", last_name: "Rogers", hourly_rate: 60)
+@store1.employees.create(first_name: "Cathy", last_name: "King", hourly_rate: 60)
+@store2.employees.create(first_name: "Sue", last_name: "Paul", hourly_rate: 60)
+@store1.employees.create(first_name: "Wendy", last_name: "Rogers", hourly_rate: 60)
+
+for employee in @store1.employees
+  puts "#{employee.first_name} #{employee.last_name}"
+end
